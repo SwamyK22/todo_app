95dcfb9727d1944afc41428ab3e41a236fe08e21
SwamyK22 <swamy199722@gmail.com>
2023-02-03 14:39:37 +0530

    basic

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@babel/eslint-parser',
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { a, b } from './app';
+
+function add(x, y) {
+  console.log(x + y);
+}
+add(a, b);
